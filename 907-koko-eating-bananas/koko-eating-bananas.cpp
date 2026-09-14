12b6bb9c7ea7940ec72ad846d34d58bc395c347a
Yash Verma <ritikverma23@gmail.com>
2026-09-14 20:36:36 +0530

    Time: 12 ms (26.00%) | Memory: 22.9 MB (58.73%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,31 +1,22 @@
 class Solution {
-  public:
-    long long f(vector<int>&v, int hour){
-        long long reqTime=0;
-        for(auto ele: v){
-            if(ele<=hour){
-                reqTime+=1;
-            }else if(ele%hour==0){
-                reqTime+=ele/hour;
-            }else{
-                reqTime=reqTime+(ele/hour)+1;
-            }
+public:
+    long f(int speed, vector<int>&v){
+        long reqTime =0;
+        for(auto ele:v){
+            reqTime+=ceil((double)ele/speed);
         }
         return reqTime;
     }
-    int minEatingSpeed(vector<int>& arr, int hour) {
-        // Code here
-        
-        long long l=1,ans=0;
-        long long r=*max_element(arr.begin(), arr.end());
-        
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1, r=*max_element(piles.begin(), piles.end());
+        int ans=0;
+
         while(l<=r){
-            long long mid=(l+r)/2;
-            
-            long long time = f(arr, mid);
-            // cout<<hour<<endl;
-            
-            if(time<=hour){
+            int mid =(l+r)/2;
+
+            long reqTime = f(mid,piles);
+
+            if(reqTime<=h){
                 ans=mid;
                 r=mid-1;
             }else{
